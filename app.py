602f899f08cf088bf251e5dba59e6a52fc525b67
Maya Ann Fetzer <mayafetzer@gmail.com>
2025-05-13 14:33:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,43 @@ import numpy as np
 import pickle
 import os
 
-# Title
-st.title("Biomass ML Model Prediction")
+# Set page config
+st.set_page_config(page_title="Biomass ML Predictor", layout="wide")
 
-# Model selection
+st.title("Biomass ML Predictor")
+
+# Define feature sets
+categorical_features = [
+    "TYPE OF BIOMASS",
+    "ADSORBENT",
+    "ADSORBATE"
+]
+
+numerical_features = [
+    "MASS OF ADSORBENT(mg/L)",
+    "VOLUME OF DYE/POLLUTANT(mL)",
+    "Ph",
+    "INITIAL CONCENTRATION OF ADSORBENT(mg/L)",
+    "CONTACT TIME(MIN)",
+    "TEMPERATURE(K)"
+]
+
+# Load pickle models
+model_dir = "./models"  # Adjust if different
 model_files = [
     "pharma_categorical.pkl",
     "pharma_no_categorical.pkl",
     "dye_categorical.pkl",
     "dye_no_categorical.pkl"
 ]
-model_choice = st.selectbox("Select a model file", model_files)
 
+# Let user pick a model
+model_choice = st.selectbox("Select a model", model_files)
+model_path = os.path.join(model_dir, model_choice)
+
+# Load the selected model
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
 # Load model
 model = None
 try:
